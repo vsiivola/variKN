fb1e97f1aab427473d0606a9de379b2bf2bbdc1c
Sami Virpioja <sami.virpioja@helsinki.fi>
2023-01-17 22:02:04 +0200

    prevent python install if wrapper cannot be compiled

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ class build_ext(build_ext_orig):
         config = 'Debug' if self.debug else 'Release'
         cmake_args = [
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
-            '-DCMAKE_BUILD_TYPE=' + config
+            '-DCMAKE_BUILD_TYPE=' + config,
+            '-DREQUIRE_PYTHON_WRAPPER=1'
         ]
 
         # example of build args
